185e9cf2faf637ff27adf76362bfcff7c1961bdd
wanderleizakaliak <wanderleizakaliak@gmail.com>
2022-01-07 13:02:58 -0300

    correção pom.xml

--- src/main/java/com/wanderlei/bookstore/BookstoreApplication.java
+++ src/main/java/com/wanderlei/bookstore/BookstoreApplication.java
@@ -1,13 +1,39 @@
 package com.wanderlei.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BookstoreApplication {
+import com.wanderlei.bookstore.domain.Categoria;
+import com.wanderlei.bookstore.domain.Livro;
+import com.wanderlei.bookstore.repositories.CategoriaRepository;
+import com.wanderlei.bookstore.repositories.LivroRepository;
 
+@SpringBootApplication
+public class BookstoreApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática", "Livro de TI");
+		
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
 	}
 
 }
