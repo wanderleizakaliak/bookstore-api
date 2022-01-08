f1e54ac6308570ae26511cb32f4d1ab7bd7e683c
wanderleizakaliak <wanderleizakaliak@gmail.com>
2022-01-08 11:48:26 -0300

    excecao personalizada para objeto nao encontrado

--- src/main/java/com/wanderlei/bookstore/service/CategoriaService.java
+++ src/main/java/com/wanderlei/bookstore/service/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.wanderlei.bookstore.domain.Categoria;
 import com.wanderlei.bookstore.repositories.CategoriaRepository;
+import com.wanderlei.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
