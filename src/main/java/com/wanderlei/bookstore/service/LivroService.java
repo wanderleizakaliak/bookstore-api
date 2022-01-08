f8b349c7a7e02c03916f636eb6fc4d6ce11ad5d7
wanderleizakaliak <wanderleizakaliak@gmail.com>
2022-01-08 16:25:07 -0300

    POST para Livro

--- src/main/java/com/wanderlei/bookstore/service/LivroService.java
+++ src/main/java/com/wanderlei/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.wanderlei.bookstore.domain.Categoria;
 import com.wanderlei.bookstore.domain.Livro;
 import com.wanderlei.bookstore.repositories.LivroRepository;
 import com.wanderlei.bookstore.service.exceptions.ObjectNotFoundException;
@@ -40,5 +41,12 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 }
