0d720a35198b3207a0db654e2d9b08ae473fbd7a
wanderleizakaliak <wanderleizakaliak@gmail.com>
2022-01-08 16:03:06 -0300

    PUT e PATCH para Livro

--- src/main/java/com/wanderlei/bookstore/resources/LivroResource.java
+++ src/main/java/com/wanderlei/bookstore/resources/LivroResource.java
@@ -6,7 +6,10 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,21 +21,33 @@ import com.wanderlei.bookstore.service.LivroService;
 @RestController
 @RequestMapping(value = "/livros")
 public class LivroResource {
-	
+
 	@Autowired
 	private LivroService service;
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Livro> findById(@PathVariable Integer id){
+	public ResponseEntity<Livro> findById(@PathVariable Integer id) {
 		Livro obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 
-	
 	@GetMapping
-	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat){
+	public ResponseEntity<List<LivroDTO>> findAll(
+			@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat) {
 		List<Livro> list = service.findAll(id_cat);
 		List<LivroDTO> lsitDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(lsitDTO);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj) {
+		Livro newObj = service.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
+	}
+
+	@PatchMapping(value = "/{id}")
+	public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj) {
+		Livro newObj = service.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
 	}
 }
--- src/main/java/com/wanderlei/bookstore/service/LivroService.java
+++ src/main/java/com/wanderlei/bookstore/service/LivroService.java
@@ -18,7 +18,7 @@ public class LivroService {
 
 	@Autowired
 	private CategoriaService categoriaService;
-	
+
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -28,5 +28,17 @@ public class LivroService {
 	public List<Livro> findAll(Integer id_cat) {
 		categoriaService.findById(id_cat);
 		return repository.findAllByCategoria(id_cat);
+	}
+
+	public Livro update(Integer id, Livro obj) {
+		Livro newObj = findById(id);
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(Livro newObj, Livro obj) {
+		newObj.setTitulo(obj.getTitulo());
+		newObj.setNome_autor(obj.getNome_autor());
+		newObj.setTexto(obj.getTexto());
 	}
 }
