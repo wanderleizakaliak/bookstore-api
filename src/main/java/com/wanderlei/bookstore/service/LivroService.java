7f424ce23af90ce8e8736dedb118e62175c7d5aa
wanderleizakaliak <wanderleizakaliak@gmail.com>
2022-01-08 16:32:49 -0300

    DELETE para Livro

--- src/main/java/com/wanderlei/bookstore/service/LivroService.java
+++ src/main/java/com/wanderlei/bookstore/service/LivroService.java
@@ -49,4 +49,9 @@ public class LivroService {
 		obj.setCategoria(cat);
 		return repository.save(obj);
 	}
+
+	public void delete(Integer id) {
+		Livro obj = findById(id);
+		repository.delete(obj);
+	}
 }
