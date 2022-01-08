acd2f337170adb0ce44c20c31b570c19f2ead16f
wanderleizakaliak <wanderleizakaliak@gmail.com>
2022-01-08 12:14:38 -0300

    find all para categoria utilizando padrao DTO

--- src/main/java/com/wanderlei/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/wanderlei/bookstore/resources/CategoriaResource.java
@@ -1,5 +1,8 @@
 package com.wanderlei.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wanderlei.bookstore.domain.Categoria;
+import com.wanderlei.bookstore.dtos.CategoriaDTO;
 import com.wanderlei.bookstore.service.CategoriaService;
 
 @RestController
@@ -16,12 +20,17 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaService service;
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Categoria> findById(@PathVariable Integer id){
+	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
 		Categoria obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-}
 
-// localhost:8080/categorias/1
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+}
